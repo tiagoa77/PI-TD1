2cedacf8f1a7fd759ca0d7dc5662d5805d9765c4
Tiago Augusto <tiago.a77@gmail.com>
2014-12-10 17:55:06 +0000

    Classes 1.2

--- src/Classes/Local.java
+++ src/Classes/Local.java
@@ -10,5 +10,37 @@ package Classes;
  * @author Tiago
  */
 public class Local {
+    private int id_local;
+    private String morada;
+    
+    public Local(){
+        this.id_local=0;
+        this.morada="";
+    }
+    
+    public Local(Local l){
+        this.id_local=l.getId_local();
+    }
+
+    public int getId_local() {
+        return id_local;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+    
+    public boolean equals(Object obj){
+        if(this==obj) return true;
+        if((obj==null) || (this.getClass() != obj.getClass()))
+            return false;
+        Local aux = (Local) obj;
+        return this.id_local == aux.getId_local();  
+    }
+    
+    public Local clone(){
+        return new Local(this);
+    }
+    
     
 }
--- src/Classes/Produto.java
+++ src/Classes/Produto.java
@@ -10,5 +10,64 @@ package Classes;
  * @author Tiago
  */
 public class Produto {
+    private int id_produto;
+    private String nome;
+    private String tipo;
+    private String descricao;
+    private int activo;
+    private int armazem_id_armazem;
     
+    public Produto(){
+        this.id_produto=0;
+        this.nome="";
+        this.tipo="";
+        this.descricao="";
+        this.activo=0;
+        this.armazem_id_armazem=0;
+    }
+    
+    public Produto(Produto p){
+        this.id_produto=p.getId_produto();
+        this.nome=p.getNome();
+        this.tipo=p.getTipo();
+        this.descricao=p.getDescricao();
+        this.activo=p.activo;
+        this.armazem_id_armazem=p.getArmazem_id_armazem();
+    }
+
+    public int getId_produto() {
+        return id_produto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public int getActivo() {
+        return activo;
+    }
+
+    public int getArmazem_id_armazem() {
+        return armazem_id_armazem;
+    }
+    
+    public boolean equals(Object obj){
+        if(this==obj) return true;
+        if((obj==null) || (this.getClass() != obj.getClass()))
+            return false;
+        Produto aux = (Produto) obj;
+        return this.id_produto == aux.getId_produto();
+    }
+    
+    public Produto clone(){
+        return new Produto(this);
+    }
 }
