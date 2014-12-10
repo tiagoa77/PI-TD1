20d3cffe9cef65b1d7bc80748c099796e13b9c0f
Tiago Augusto <tiago.a77@gmail.com>
2014-12-10 19:56:12 +0000

    Revert "Revert "Classes 1.3""
    This reverts commit 42c2d28b151073be67d86743286a1965ffb4ab0e.

--- src/Classes/Cliente.java
+++ src/Classes/Cliente.java
@@ -5,10 +5,90 @@
  */
 package Classes;
 
+import java.util.HashMap;
+
 /**
  *
  * @author Tiago
  */
 public class Cliente {
+    private int id_cliente;
+    private String nome_farmacia;
+    private String nome_farmaceutico;
+    private int contacto;
+    private int nif;
+    private int activo;
+    private int local_id_local;
+    private HashMap<Integer,Encomenda> encomendas;
     
+    public Cliente(){
+        this.id_cliente=0;
+        this.nome_farmacia="";
+        this.nome_farmaceutico="";
+        this.contacto=0;
+        this.nif=0;
+        this.activo=0;
+        this.local_id_local=0;
+        this.encomendas=new HashMap<>();
+    }
+    
+    public Cliente(Cliente c){
+        this.id_cliente=c.getId_cliente();
+        this.nome_farmacia=c.getNome_farmacia();
+        this.nome_farmaceutico=c.getNome_farmaceutico();
+        this.contacto=c.getContacto();
+        this.nif=c.getNif();
+        this.activo=c.getActivo();
+        this.local_id_local=c.getLocal_id_local();
+        this.encomendas=c.getEncomendas();
+        
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public String getNome_farmacia() {
+        return nome_farmacia;
+    }
+
+    public String getNome_farmaceutico() {
+        return nome_farmaceutico;
+    }
+
+    public int getContacto() {
+        return contacto;
+    }
+
+    public int getNif() {
+        return nif;
+    }
+
+    public int getActivo() {
+        return activo;
+    }
+
+    public int getLocal_id_local() {
+        return local_id_local;
+    }
+
+    public HashMap<Integer, Encomenda> getEncomendas() {
+        HashMap<Integer, Encomenda> aux = new HashMap<Integer, Encomenda>();
+        for(Encomenda e : this.encomendas.values())
+            aux.put(e.getId_encomenda(), e.clone());
+        return aux;
+    }
+    
+    
+    public boolean equals(Object obj){
+        if(this==obj) return true;
+        if((obj==null) || (this.getClass() != obj.getClass()))
+            return false;
+        Cliente aux = (Cliente) obj;
+        return this.id_cliente == aux.getId_cliente();
+    }
+    
+    public Cliente clone(){
+        return new Cliente(this);
+    }
 }
